dacbe752dcb518ef17d4c5560a3309dd619dbec8
Tian ZHANG <769606726@qq.com>
2021-09-13 10:32:14 +0800

    Update run-go-build.sh

--- run-go-build.sh
+++ run-go-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 FILES=$(go list ./...  | grep -v /vendor/)
-exec go build -tags dynamic $FILES
+# exec go build -tags dynamic $FILES 会失败不知道为啥。。
+exec go build -tags=dynamic $FILES
