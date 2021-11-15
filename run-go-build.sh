155487df5e16b28b9b7632fddab28bba118937b9
ztsong <ztsong@huaxingsec.com>
2021-11-15 13:07:57 +0800

    feat: put at head

--- run-go-build.sh
+++ run-go-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 FILES=$(go list ./...  | grep -v /vendor/)
 # exec go build -tags dynamic $FILES 会失败不知道为啥。。
-exec CGO_ENABLED=1 GOARCH=arm64 go build -tags=dynamic $FILES
+CGO_ENABLED=1 GOARCH=arm64 exec go build -tags=dynamic $FILES
