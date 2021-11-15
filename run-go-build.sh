96316a0285255ab856c5d2d4ac50d3321347f908
ztsong <ztsong@huaxingsec.com>
2021-11-15 13:01:50 +0800

    feat: add go arch for m1

--- run-go-build.sh
+++ run-go-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 FILES=$(go list ./...  | grep -v /vendor/)
 # exec go build -tags dynamic $FILES 会失败不知道为啥。。
-exec go build -tags=dynamic $FILES
+exec GOARCH=arm64 go build -tags=dynamic $FILES
